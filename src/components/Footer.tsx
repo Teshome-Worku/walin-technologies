22351b85d6629cb1da46ae67ecc49e855590ca4b
Teshome-worku <teshomeworku96@gmail.com>
2026-04-10 14:30:27 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,8 @@ import facebook from '@/icons/facebook.png';
 import telegram from '@/icons/telegram.png'
 import linkedin from '@/icons/linkedin.png';
 import email from '@/icons/email.png';
-import upArrow from '@/icons/up-arrow.png'
+import upArrow from '@/icons/up-arrow.png';
+import whatsapp from '@/icons/whastapp.png';
 import Link from "next/link";
 
 export default function Footer() {
@@ -36,20 +37,24 @@ export default function Footer() {
                     <p className="mt-3 text-sm text-gray-400 dark:text-gray-400">
                         Helping businesses move from manual work to smart digital solutions.
                     </p>
-                    <div className="mt-5 flex items-center justify-center gap-5 md:justify-start">
+                    <div className="mt-5 flex items-center justify-center gap-5 md:justify-start ">
+                        <Link href="https://wa.me/251955800626" target="_blank" aria-label="WhatsApp" className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-gray-700 text-gray-300 transition hover:border-primary/40 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-primary/60 ">
+                            <Image src={whatsapp} alt="Linkedin icon" title="WhatsApp" className="hover:scale-105 transition duration-400" />
+
+                        </ Link>
                         <Link href="https://www.linkedin.com/in/teshome-worku-017834392" target="_blank" aria-label="LinkedIn" className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-gray-700 text-gray-300 transition hover:border-primary/40 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-primary/60">
-                            <Image src={linkedin} alt="Linkedin icon" title="Linkedin" />
+                            <Image src={linkedin} alt="Linkedin icon" title="LinkedIn" className="hover:scale-105 transition duration-400" />
 
                         </ Link>
                         <Link href="mailto:walintech22@gmail.com" aria-label="Email" target="_blank" className="inline-flex h-8 w-8 items-center justify-center rounded-full  text-gray-300 transition hover:border-primary/40 hover:text-white focus:outline-none  focus-visible:ring-primary/60">
-                            <Image src={email} alt="Email icon" title="Email" />
+                            <Image src={email} alt="Email icon" title="Email" className="hover:scale-105 transition duration-400" />
                         </ Link>
                         <Link href="https://t.me/walintech22" aria-label="Telegram" target="_blank" className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-gray-700 text-gray-300 transition hover:border-primary/40 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-primary/60 ">
-                            <Image src={telegram} alt="Telegram icon" title="Telegram" />
+                            <Image src={telegram} alt="Telegram icon" title="Telegram" className="hover:scale-105 transition duration-400" />
 
                         </Link>
                         <Link href="https://web.facebook.com/walintech" aria-label="Facebook" target="_blank" className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-gray-700 text-gray-300 transition hover:border-primary/40 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-primary/60">
-                            <Image src={facebook} alt="facebook icon" title="Facebook" />
+                            <Image src={facebook} alt="facebook icon" title="Facebook" className="hover:scale-105 transition duration-400" />
 
                         </Link>
                     </div>
